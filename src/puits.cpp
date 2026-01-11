f7dbc750998e81d18f749aca7141d474aba9df74
kombila <kombila@kombila.local>
2026-01-11 13:01:29 +0100

    Modification du constructeur et methode dessiner

--- src/puits.cpp
+++ src/puits.cpp
@@ -1,6 +1,34 @@
 #include "puits.h"
 
-Puits::Puits()
+Puits::Puits(int h, int l): ObjetGraphique({0,0},15)
 {
+    hauteur = h;
+    largeur = l;
+
+    for(int i=0; i<hauteur; i++)
+    {
+        for(int j =0; j<largeur;j++)
+        {
+
+            Mat[i][j] = 0;
+
+        }
+    }
 
 }
+
+
+void Puits::dessiner(QPainter* p)
+{
+    for(int i = 0; i < hauteur; i++)
+    {
+        for(int j = 0; j < largeur; j++)
+        {
+            if(Mat[i][j] != 0)
+            {
+                p->fillRect(coordonnes.x + j * 15,coordonnes.y + i * 15,15,15, QBrush(Qt::gray));
+            }
+        }
+    }
+}
+
